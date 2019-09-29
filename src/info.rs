c5c7ad1fc636261c0c0bec8eaf9d141849bb292a
rumblefrog <contact@zhenyangli.me>
2019-09-29 18:23:40 -0400

    a2s info

---
+++ src/info.rs
@@ -0,0 +1,252 @@
+use std::io::{Cursor, ErrorKind};
+use std::io::Write;
+use std::net::ToSocketAddrs;
+
+use byteorder::{LittleEndian, ReadBytesExt};
+
+use crate::{A2SClient, ReadCString};
+use crate::errors::{Error, Result};
+
+pub struct Info {
+    // Protocol version used by the server.
+    pub protocol: u8,
+
+    // Name of the server.
+    pub name: String,
+
+    // Map the server has currently loaded.
+    pub map: String,
+
+    // Name of the folder containing the game files.
+    pub folder: String,
+
+    // Full name of the game.
+    pub game: String,
+
+    // Steam Application ID of game.
+    pub app_id: u16,
+
+    // Number of players on the server.
+    pub players: u8,
+
+    // Maximum number of players the server reports it can hold.
+    pub max_players: u8,
+
+    // Number of bots on the server.
+    pub bots: u8,
+
+    // Indicates the type of server
+	// Rag Doll Kung Fu servers always return 0 for "Server type."
+    pub server_type: ServerType,
+
+    // Indicates the operating system of the server
+    pub server_os: ServerOS,
+
+    // Indicates whether the server requires a password
+    pub visibility: bool,
+
+    // Specifies whether the server uses VAC
+    pub vac: bool,
+
+    // These fields only exist in a response if the server is running The Ship
+    pub the_ship: Option<TheShip>,
+
+    // Version of the game installed on the server.
+    pub version: String,
+
+    // If present, this specifies which additional data fields will be included.
+    pub edf: u8,
+
+    pub extended_server_info: ExtendedServerInfo,
+
+    // Available if edf & 0x40 is true
+    pub source_tv: Option<SourceTVInfo>,
+}
+
+pub struct TheShip {
+    // Indicates the game mode
+    pub mode: TheShipMode,
+
+    // The number of witnesses necessary to have a player arrested.
+    pub witnesses: u8,
+
+    // Time (in seconds) before a player is arrested while being witnessed.
+    pub duration: u8 ,
+}
+
+pub enum TheShipMode {
+    Hunt,
+    Elimination,
+    Duel,
+    Deathmatch,
+    VIPTeam,
+    TeamElimination,
+    Unknown,
+}
+
+impl From<u8> for TheShipMode {
+    fn from (v: u8) -> Self {
+        match v {
+            0 => Self::Hunt,
+            1 => Self::Elimination,
+            2 => Self::Duel,
+            3 => Self::Deathmatch,
+            4 => Self::VIPTeam,
+            5 => Self::TeamElimination,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+pub struct ExtendedServerInfo {
+    // The server's game port number.
+    // Available if edf & 0x80 is true
+    pub port: Option<u16>,
+
+    // Server's SteamID.
+    // Available if edf & 0x10 is true
+    pub steam_id: Option<u64>,
+
+    // Tags that describe the game according to the server (for future use.)
+    // Available if edf & 0x20 is true
+    pub keywords: Option<String>,
+
+    // The server's 64-bit GameID. If this is present, a more accurate AppID is present in the low 24 bits.
+    // The earlier AppID could have been truncated as it was forced into 16-bit storage.
+    // Avaialble if edf & 0x01 is true
+    pub game_id: Option<u64>,
+}
+
+pub struct SourceTVInfo {
+    // Spectator port number for SourceTV.
+    pub port: u16,
+
+    // Name of the spectator server for SourceTV.
+    pub name: String,
+}
+
+pub enum ServerType {
+    Dedicated,
+    NonDedicated,
+    SourceTV,
+}
+
+pub enum ServerOS {
+    Linux,
+    Windows,
+    Mac,
+}
+
+impl A2SClient {
+    pub fn info<A: ToSocketAddrs>(&self, addr: A) -> Result<Info> {
+        let mut cursor = Cursor::new(vec![0u8, 25]);
+
+        cursor.write(&[0xff, 0xff, 0xff, 0xff])?;
+        cursor.write(&[0x54])?;
+        cursor.write(b"Source Engine Query")?;
+        cursor.write(&[0x00])?;
+
+        let data = self.send(cursor.get_ref(), addr)?;
+        let mut data = Cursor::new(data);
+
+        if data.read_u8()? != 0x49u8 {
+            return Err(Error::InvalidResponse);
+        }
+
+        let app_id: u16;
+        let mut flag = 0u8;
+
+        Ok(Info{
+            protocol: data.read_u8()?,
+            name: data.read_cstring()?,
+            map: data.read_cstring()?,
+            folder: data.read_cstring()?,
+            game: data.read_cstring()?,
+            app_id: {
+                app_id = data.read_u16::<LittleEndian>()?;
+
+                app_id
+            },
+            players: data.read_u8()?,
+            max_players: data.read_u8()?,
+            bots: data.read_u8()?,
+            server_type: {
+                match data.read_u8()? as char {
+                    'd' => ServerType::Dedicated,
+                    'i' => ServerType::NonDedicated,
+                    'p' => ServerType::SourceTV,
+                    _ => return Err(Error::Other("Invalid server type"))
+                }
+            },
+            server_os: {
+                match data.read_u8()? as char {
+                    'l' => ServerOS::Linux,
+                    'w' => ServerOS::Windows,
+                    'm' | 'o' => ServerOS::Mac,
+                    _ => return Err(Error::Other("Invalid environment"))
+                }
+            },
+            visibility: data.read_u8()? != 0,
+            vac: data.read_u8()? != 0,
+            the_ship: {
+                if app_id == 2400 {
+                    Some(TheShip{
+                        mode: TheShipMode::from(data.read_u8()?),
+                        witnesses: data.read_u8()?,
+                        duration: data.read_u8()?,
+                    })
+                } else {
+                    None
+                }
+            },
+            version: data.read_cstring()?,
+            edf: {
+                match data.read_u8() {
+                    Ok(val) => { flag = val; },
+                    Err(err) => { if err.kind() != ErrorKind::UnexpectedEof { return Err(Error::Io(err)); }}
+                }   
+                flag
+            },
+            extended_server_info: ExtendedServerInfo{
+                port: {
+                    if flag & 0x80 != 0 {
+                        Some(data.read_u16::<LittleEndian>()?)
+                    } else {
+                        None
+                    }
+                },
+                steam_id: {
+                    if flag & 0x10 != 0 { 
+                        Some(data.read_u64::<LittleEndian>()?)
+                    } else {
+                        None
+                    }
+                },
+                keywords: {
+                    if flag & 0x20 != 0 {
+                        Some(data.read_cstring()?)
+                    } else {
+                        None
+                    }
+                },
+                game_id: {
+                    if flag & 0x01 != 0 {
+                        Some(data.read_u64::<LittleEndian>()?)
+                    } else {
+                        None
+                    }
+                }
+            },
+            source_tv: {
+                if flag & 0x40 != 0 {
+                    Some(SourceTVInfo{
+                        port: data.read_u16::<LittleEndian>()?,
+                        name: data.read_cstring()?,
+                    })
+                } else {
+                    None
+                }
+            }
+        })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,155 @@
-pub mod client;
+pub mod info;
 pub mod errors;
+
+use std::net::{UdpSocket, ToSocketAddrs};
+use std::time::Duration;
+use std::io::{Read, Cursor};
+use std::ops::Deref;
+
+use byteorder::{ByteOrder, LittleEndian};
+use bzip2::read::{BzDecoder};
+use crc::{crc32};
+
+use crate::errors::{ Result, Error };
+
+const SINGLE_PACKET: i32 = -1;
+const MULTI_PACKET: i32 = -2;
+
+struct PacketFragment {
+    number: u8,
+    payload: Vec<u8>,
+}
+
+pub struct A2SClient {
+    socket: UdpSocket,
+    max_size: usize,
+}
+
+impl A2SClient {
+    pub fn new(max_size: Option<usize>) -> Result<A2SClient> {
+        let socket = UdpSocket::bind("0.0.0.0:0")?;
+
+        socket.set_read_timeout(Some(Duration::new(5, 0)))?;
+        socket.set_write_timeout(Some(Duration::new(5, 0)))?;
+
+        Ok(A2SClient{
+            socket: socket,
+            max_size: max_size.unwrap_or(1400),
+        })
+    }
+
+    fn send<A: ToSocketAddrs>(&self, payload: &[u8], addr: A) -> Result<Vec<u8>> {
+        self.socket.send_to(payload, addr)?;
+
+        // 32 Bit (4 bytes) header to indicate split or singular response
+        let mut header = [0; 4];
+
+        self.socket.recv(&mut header)?;
+
+        let header = LittleEndian::read_i32(&header);
+
+        if header == SINGLE_PACKET {
+            let mut data = vec![0u8; self.max_size];
+
+            let read = self.socket.recv(&mut data)?;
+
+            data.truncate(read);
+
+            Ok(data)
+        } 
+
+        else if header == MULTI_PACKET {
+            // ID - long (4 bytes)
+            // Total - byte (1 byte)
+            // Number - byte (1 byte)
+            // Size - short (2 bytes)
+
+            let mut data = [0u8, 8];
+
+            self.socket.recv(&mut data)?;
+
+            let id = LittleEndian::read_i32(&data[0..4]);
+            let total_packets: usize = data[4] as usize;
+            let switching_size: usize = LittleEndian::read_i16(&data[6..8]) as usize;
+
+            let mut packets: Vec<PacketFragment> = Vec::with_capacity(total_packets);
+
+            loop {
+                let mut data = vec![0u8; switching_size];
+
+                let read = self.socket.recv(&mut data)?;
+
+                if read < data.len() {
+                    data.truncate(read);
+                }
+
+                // Skip header field (4 bytes 0..4)
+                let packet_id = LittleEndian::read_i32(&data[4..8]);
+
+                if packet_id != id {
+                    return Err(Error::MismatchID);
+                }
+
+                packets.push(PacketFragment{
+                    number: data[10],
+                    payload: Vec::from(&data[12..]),
+                });
+
+                if packets.len() == total_packets {
+                    break;
+                }
+            }
+
+            packets.sort_by_key(|p| p.number);
+
+            let mut aggregation = Vec::with_capacity(total_packets * self.max_size);
+
+            for p in packets {
+                aggregation.extend(p.payload);
+            }
+
+            if id as u32 & 0x80000000 != 0 {
+                let decompressed_size = LittleEndian::read_i32(&data[0..4]);
+                let checksum = LittleEndian::read_i32(&data[4..8]);
+
+                if decompressed_size > (1024*1024) {
+                    return Err(Error::InvalidBz2Size);
+                }
+
+                let mut decompressed = Vec::with_capacity(total_packets * self.max_size);
+
+                BzDecoder::new(aggregation.deref()).read(&mut decompressed)?;
+
+                if crc32::checksum_ieee(&decompressed) != checksum as u32 {
+                    return Err(Error::CheckSumMismatch);
+                }
+
+                Ok(decompressed)
+            } 
+            
+            else {
+                Ok(aggregation)
+            }
+        }
+
+        else {
+            Err(Error::InvalidResponse)
+        }
+    }
+}
+
+trait ReadCString {
+    fn read_cstring(&mut self) -> Result<String>;
+}
+
+impl ReadCString for Cursor<Vec<u8>> {
+    fn read_cstring(&mut self) -> Result<String> {
+        let mut buf = [0; 1];
+        let mut str_vec = Vec::with_capacity(256);
+        loop {
+            self.read(&mut buf)?;
+            if buf[0] == 0 { break; } else { str_vec.push(buf[0]); }
+        }
+        Ok(String::from_utf8_lossy(&str_vec[..]).into_owned())
+    }
+}
