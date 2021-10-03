b9243b0f5f417d631c46cb085c2d5f914093fa0c
Markus Reiter <me@reitermark.us>
2021-10-03 09:44:35 +0200

    Allow generating rules response.

--- src/rules.rs
+++ src/rules.rs
@@ -26,6 +26,31 @@ pub struct Rule {
 }
 
 impl Rule {
+  pub fn vec_to_bytes(rules: Vec<Self>) -> Vec<u8> {
+      let mut bytes = Vec::new();
+
+      bytes.extend(&[0xff, 0xff, 0xff, 0xff, 0x45]);
+
+      bytes.extend(rules.len().to_le_bytes());
+
+      for rule in rules {
+          bytes.extend(rule.to_bytes());
+      }
+
+      bytes
+  }
+
+  pub fn to_bytes(&self) -> Vec<u8> {
+      let mut bytes = Vec::new();
+
+      bytes.extend(self.name.as_bytes());
+      bytes.push(0);
+      bytes.extend(self.value.as_bytes());
+      bytes.push(0);
+
+      bytes
+  }
+
   pub fn from_cursor(mut data: Cursor<Vec<u8>>) -> Result<Vec<Self>> {
       if data.read_u8()? != 0x45 {
           return Err(Error::InvalidResponse);
